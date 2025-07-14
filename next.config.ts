b02b2479d4b17e313b2d7601e118e392cfad3e36
nick <nicholasvannguyen@yahoo.com.au>
2025-07-14 16:52:56 +0800

    feat(config): add image domains for Next.js configuration and update dependencies
    - Configured Next.js to allow images from specified domains, enhancing image handling capabilities.
    - Added SWR library as a dependency for improved data fetching and state management.
    - Updated package-lock.json to reflect the addition of new dependencies.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: [
+      "oeqtynbxxaaskcuqgcbc.supabase.co", 
+    ],
+  },
 };
 
 export default nextConfig;
